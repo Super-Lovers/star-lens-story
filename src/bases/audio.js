683fe6bab619ca68a4862732775b8be6e7179d65
Super-Lovers <n.v.ivanov@st.hanze.nl>
2020-08-08 19:02:41 +0300

    Added explosion/button sound effects + changed title btn

---
+++ src/bases/audio.js
@@ -0,0 +1,7 @@
+var sound_effects_volume = 0.3;
+
+SimpleAudio.tracks.add("boom", "audio/sounds/explosion.wav");
+
+// Buttons
+SimpleAudio.tracks.add("start_button", "audio/sounds/start_button.wav");
+SimpleAudio.tracks.add("button", "audio/sounds/button.wav");
