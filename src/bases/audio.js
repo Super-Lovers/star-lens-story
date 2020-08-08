96eb71b64de5aa708caeddacb6e171ebc243fafb
Super-Lovers <n.v.ivanov@st.hanze.nl>
2020-08-08 21:47:05 +0300

    Added sound effect to console output

--- src/bases/audio.js
+++ src/bases/audio.js
@@ -1,6 +1,8 @@
 var sound_effects_volume = 0.3;
 
+// Console sounds
 SimpleAudio.tracks.add("boom", "audio/sounds/explosion.wav");
+SimpleAudio.tracks.add("console_output", "audio/sounds/console_output.wav");
 
 // Buttons
 SimpleAudio.tracks.add("start_button", "audio/sounds/start_button.wav");
