d2688b582c1940523b6ffb3b552a3eab4ad9e9ae
Super-Lovers <n.v.ivanov@st.hanze.nl>
2020-08-09 21:20:02 +0300

    Wrote 3.0 ambush ending

--- src/bases/audio.js
+++ src/bases/audio.js
@@ -5,5 +5,4 @@ SimpleAudio.tracks.add("boom", "audio/sounds/explosion.wav");
 SimpleAudio.tracks.add("console_output", "audio/sounds/console_output.wav");
 
 // Buttons
-SimpleAudio.tracks.add("start_button", "audio/sounds/start_button.wav");
 SimpleAudio.tracks.add("button", "audio/sounds/button.wav");
